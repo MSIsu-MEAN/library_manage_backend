cf577a2fe40d888214da465e06243fe39ade3154
MSIsu-MEAN <iswaryams97@gmail.com>
2024-10-07 14:33:30 +0530

    Update users.test.js

--- users.test.js
+++ users.test.js
@@ -6,7 +6,7 @@ describe('POST /users/addBooks', () => {
     it('should add a book and return 201 status', async () => {
         const response = await request(app).post('/users/addBooks');
         expect(response.status).toBe(200);
-        expect(response.body).toEqual({ title: '1', author: 'John Doe',description:"new content given",publicationYear:"2024",ISBN:"84-12-45454-55" });
+        expect(response.body).toEqual({ title: '1', author: 'John',description:"new content given",publicationYear:"2024",ISBN:"84-12-45454-55" });
     });
 
     it('should return 404 if the user is not found', async () => {

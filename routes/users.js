3cf68cb9ecfd2b23f01f38d3a9a9d83ad94cf85e
MSIsu-MEAN <iswaryams97@gmail.com>
2024-10-07 14:32:34 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/signIn',controller.signIn)
 router.post('/addBooks',controller.addBooks)
 router.post('/particularList',controller.particularList)
 router.post('/booksList',controller.booksList)
